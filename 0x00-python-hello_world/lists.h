da16a24cecd6f05e4806e4ec7c7c39ae6c28cc75
adexino0606 <eazi0606@gmail.com>
2022-05-30 12:27:12 -0700

    lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -16,9 +16,7 @@ typedef struct listint_s
         int n;
         struct listint_s *next;
 } listint_t;
-
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
+                                                  size_t print_listint(const listint_t *h);         listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
