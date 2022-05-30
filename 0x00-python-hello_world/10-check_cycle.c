45cefbe9f841d783c12d89356c616e8ff95ebb18
adexino0606 <eazi0606@gmail.com>
2022-05-30 07:14:49 -0700

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,39 +1,43 @@
 #include "lists.h"
 
 /**
- * check_cycle - Check if a linked list have a cycle.
- *
- * @list: Pointer to a linked list.
- *
- * Return: 0 if there is no cycle, 1 if there is a cycle.
+ * check_cycle - checks if a singly linked list has
+ * a cycle in it
+ * @list: pointer to the list
+ * Return: 0 if there is no cycle,
+ * 1 if there is a cycle
  */
-
 int check_cycle(listint_t *list)
 {
-	listint_t *fast_node = NULL, *slow_node = NULL;
-	int flag = 1;
+	listint_t *p2;
+	listint_t *prev;
 
-	if (!list)
-		return (0);
-
-	fast_node = list, slow_node = list;
-
-	while (!(fast_node == slow_node) || flag)
+	p2 = list;
+	prev = list;
+	while (list && p2 && p2->next)
 	{
-		flag = 0;
+		list = list->next;
+		p2 = p2->next->next;
 
-		if (fast_node->next != NULL)
-			fast_node = fast_node->next;
-		else
-			return (0);
+		if (list == p2)
+		{
+			list = prev;
+			prev =  p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != list && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == list)
+					break;
 
-		if (fast_node->next != NULL)
-			fast_node = fast_node->next;
-		else
-			return (0);
-
-		slow_node = slow_node->next;
+				list = list->next;
+			}
+			return (1);
+		}
 	}
 
-	return (1);
+	return (0);
 }
