9e013ca3426d9ade1e9a72383a0e98244648e7e6
adexino0606 <eazi0606@gmail.com>
2022-05-30 06:41:07 -0700

    list.h

--- 0x00-python-hello_world/list.h
+++ 0x00-python-hello_world/list.h
@@ -5,11 +5,12 @@
 
 /**
  * struct listint_s - singly linked list
+ *
  * @n: integer
+ *
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct listint_s
 {
