bc19ad94164320ee1f1a28e1b0404c55262f02a2
adexino0606 <eazi0606@gmail.com>
2022-05-31 04:27:52 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ch in range(0, -26, -1):
-    print(f"{chr(122 + ch) if (-ch % 2) == 0 else chr(ch - 32 + 122)}", end="")
+for ch in reversed(range(97, 123)):
+    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
