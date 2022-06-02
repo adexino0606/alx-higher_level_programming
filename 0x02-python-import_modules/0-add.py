8fcb5a53ae414b7ff0c28799e80462bde6c46eb7
adexino0606 <eazi0606@gmail.com>
2022-06-02 06:25:41 -0700

    0-import_add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    print(f"{a} + {b} = {add(a, b)}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
