4119d12b0d51f113ed0a04864ee6c34a93c7500c
adexino0606 <adexino0606@github.com>
2022-08-10 21:51:52 -0700

    hmmm

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Script that creates an user in MySQL server
--- Query to create the user 'user_0d_1' in MySQL server
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Creates the MySQL server user user_0d_1.
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
